c9cdcd584cc1ffc250568769575a0c2eba0f5603
Gaston Sanchez <43063424+GastonSanchez@users.noreply.github.com>
2024-05-31 19:13:33 -0300

    Separo normalizacion de login

--- js/Normalizacion.js
+++ js/Normalizacion.js
@@ -1,30 +1,19 @@
-const { log } = require("console");
-const { response } = require("express");
-const SmartyStreetsSDK = require("smartystreets-javascript-sdk");
-const SmartyStreetsCore = SmartyStreetsSDK.core;
-const Lookup = SmartyStreetsSDK.usStreet.Lookup;
-//configuraciones para pder usar SDK
 
-let authId = process.env.SMARTY_AUTH_TOKEN;
-let authToken = process.env.SMARTY_AUTH_TOKEN;
-const credentials = new SmartyStreetsCore.StaticCredentials (authId, authToken);
-//creamos una instancia de cliente y variables de entorno
+document.getElementById('formulario').addEventListener('submit', function(event) {
+event.preventDefault();
 
-let client = SmartyStreetsCore.buildClient.usStreet(credentials);
+const direccion = document.getElementById('direccionemprendimiento').value;
 
-let lookup = new Lookup ("1600 Amphiteatre Parwkay Mountain View, CA 94043");
 
-client.send(lookup)
-    .then(handlaSuccess)
-    .catch(handleError);
-
-function handlaSuccess(response){
-    var result = response.lookup[0].result[0];
-    console.log("Adress:  " + result.delieryLine1 + " " + result.lastLine);
-    console.log("Latitude:  " + result.metadata.latitude);
-    console.log("Longitude:  " + result.metadata.longitude);
-    }
-
-function handleError (response){
-    console.log (response);
+fetch(`http://servicios.usig.buenosaires.gob.ar/normalizar/?direccion=${direccion}`)
+.then(response => response.json())
+.then(data => {
+    if (window.confirm("¿es tu direccion " + data.direccionesNormalizadas[0].direccion + " ?")){
+    window.location.replace("index.html");
 }
+})
+.catch(error => console.error('Error:', error));
+}
+
+);
+
