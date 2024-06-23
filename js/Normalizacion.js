fee6ff795ed88ed3d01d9d405fa39689c10d2ed2
Matias Remon <matiasremon@gmail.com>
2024-06-23 14:18:33 -0300

    estilos y normalización

--- js/Normalizacion.js
+++ js/Normalizacion.js
@@ -2,17 +2,36 @@
 document.getElementById('formulario').addEventListener('submit', function(event) {
 event.preventDefault();
 
-const direccion = document.getElementById('direccionemprendimiento').value;
+const direccionEmprendimiento = document.getElementById('direccionemprendimiento').value;
+const direccionParticular = document.getElementById('direccionparticular').value;
 
 
-fetch(`http://servicios.usig.buenosaires.gob.ar/normalizar/?direccion=${direccion}`)
+if(direccionEmprendimiento!=""){
+fetch(`http://servicios.usig.buenosaires.gob.ar/normalizar/?direccion=${direccionEmprendimiento}`)
 .then(response => response.json())
 .then(data => {
-    if (window.confirm("¿es tu direccion " + data.direccionesNormalizadas[0].direccion + " ?")){
+    for (let i=0;i<data.direccionesNormalizadas.length;i++){
+    if (window.confirm("¿es la direccion de tu emprendimiento " + data.direccionesNormalizadas[i].direccion + " ?")){
     window.location.replace("index.html");
+    break;
+}
 }
 })
 .catch(error => console.error('Error:', error));
+}
+
+fetch(`http://servicios.usig.buenosaires.gob.ar/normalizar/?direccion=${direccionParticular}`)
+.then(response => response.json())
+.then(data => {
+    for (let i=0;i<data.direccionesNormalizadas.length;i++){
+    if (window.confirm("¿es tu direccion particular " + data.direccionesNormalizadas[i].direccion + " ?")){
+    window.location.replace("index.html");
+    break;
+}
+}
+})
+.catch(error => console.error('Error:', error));
+
 }
 
 );
