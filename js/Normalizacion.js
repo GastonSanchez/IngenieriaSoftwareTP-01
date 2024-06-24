691ab8bbd6486dc316f6d63f9b4b7a1d85c1e00c
Gaston Sanchez <43063424+GastonSanchez@users.noreply.github.com>
2024-06-24 00:15:37 -0300

    normalizacion con desplegable de direcciones

--- js/Normalizacion.js
+++ js/Normalizacion.js
@@ -1,38 +1,56 @@
 
 document.getElementById('formulario').addEventListener('submit', function(event) {
-event.preventDefault();
-
-const direccionEmprendimiento = document.getElementById('direccionemprendimiento').value;
-const direccionParticular = document.getElementById('direccionparticular').value;
-
-
-if(direccionEmprendimiento!=""){
-fetch(`http://servicios.usig.buenosaires.gob.ar/normalizar/?direccion=${direccionEmprendimiento}`)
-.then(response => response.json())
-.then(data => {
-    for (let i=0;i<data.direccionesNormalizadas.length;i++){
-    if (window.confirm("¿es la direccion de tu emprendimiento " + data.direccionesNormalizadas[i].direccion + " ?")){
+    event.preventDefault();
+    alert("Registro realizado con exito!");
     window.location.replace("index.html");
-    break;
-}
-}
-})
-.catch(error => console.error('Error:', error));
+});
+
+function consultarAPI(direccion, select) {
+    
+    select.innerHTML = ""; 
+    select.style.display="block";
+    const confirmaDireccion = document.createElement("option");
+    confirmaDireccion.value="";
+    confirmaDireccion.textContent = "Seleccione su direccion correcta";
+    select.appendChild(confirmaDireccion);
+    
+    fetch(`http://servicios.usig.buenosaires.gob.ar/normalizar/?direccion=${direccion}`)
+    .then(response => response.json())
+    .then(data => {
+        for (let i=0;i<data.direccionesNormalizadas.length;i++){
+            const option = document.createElement("option");
+            option.value = data.direccionesNormalizadas[i].direccion;
+            option.textContent = data.direccionesNormalizadas[i].direccion;
+            select.appendChild(option);
+        }
+    }).catch(error => console.error('Error:', error));
 }
 
-fetch(`http://servicios.usig.buenosaires.gob.ar/normalizar/?direccion=${direccionParticular}`)
-.then(response => response.json())
-.then(data => {
-    for (let i=0;i<data.direccionesNormalizadas.length;i++){
-    if (window.confirm("¿es tu direccion particular " + data.direccionesNormalizadas[i].direccion + " ?")){
-    window.location.replace("index.html");
-    break;
-}
-}
-})
-.catch(error => console.error('Error:', error));
+document.addEventListener("DOMContentLoaded", () => {
+    const inputDireccionParticular = document.getElementById("direccionparticular");
 
-}
+    inputDireccionParticular.addEventListener("change", () => {
+        const direccionParticular = inputDireccionParticular.value.trim();
+        const select = document.getElementById("direccionesParticulares");
+        if (direccionParticular !== "") {
+            try {
+                consultarAPI(direccionParticular, select);
+            } catch (error) {
+                console.error("Error al consultar la API:", error);
+            }
+        }
+    });
 
-);
-
+    const imputDireccionEmprendimiento = document.getElementById("direccionemprendimiento");
+    imputDireccionEmprendimiento.addEventListener("change", () => {
+        const direccionEmprendimiento = imputDireccionEmprendimiento.value.trim();
+        const select = document.getElementById("direccionesEmprendimientos");
+        if (direccionEmprendimiento !== "") {
+            try {
+                consultarAPI(direccionEmprendimiento, select);
+            } catch (error) {
+                console.error("Error al consultar la API:", error);
+            }
+        }
+    });
+});
